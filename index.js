fa490a01768df93602bfa8bd545a85caff29c972
Heather Christman <efl7a@yahoo.de>
2018-04-17 12:09:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+function findMatching(group, name) {
+  return group.filter(function (driver) {return driver.toLowerCase() == name.toLowerCase(); })
+}
+function fuzzyMatch(group, name) {
+  return group.filter(function (driver) {return driver[0] === name[0]; })
+}
+
+function matchName(group, name) {
+  return group.filter(function (driver) {return driver.name == name; })
+}

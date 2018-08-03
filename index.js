88a4a8c79c906d79b35da7efa19ffc11ce2191f4
Tao <werewaswas@live.com>
2018-08-03 02:06:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, match) {
+  return array.filter(function (element) {
+    return element == match || element == match.toLowerCase()
+  })
+}
+
+function fuzzyMatch(array, letters) {
+  const lettersLength = letters.length
+  return array.filter(function (element) {
+    return element.slice(0, lettersLength) == letters
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (element) {
+    return element.name == name
+  })
+}

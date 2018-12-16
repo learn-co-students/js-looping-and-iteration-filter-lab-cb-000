038c1fd7ae4882dda8fd7fb5c5d206b53978c98e
Elizabeth J <liz.jenerson@gmail.com>
2018-12-16 07:20:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+
+	const matches = drivers.filter(function(driver) {return driver.toLowerCase() === name.toLowerCase();});
+	//you can also use RegEx to compare driver and name when they don't
+	//have the same case
+	
+	return matches;
+}
+
+function fuzzyMatch(drivers, name){
+	//returns all drivers whose names begin with the provided letters.
+	//consider using some RegEx here
+
+}

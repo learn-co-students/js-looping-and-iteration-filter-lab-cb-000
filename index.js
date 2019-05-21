8ec73ebf286db10cd4bb93f02671e8a578fd7400
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-21 12:42:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (item) {return item.toLowerCase() === name.toLowerCase();});
+}
+function fuzzyMatch(array, letters) {
+  return array.filter(function (item) {return item.slice(0, letters.length) === letters;});
+}
+function matchName(array, string) {
+  return array.filter(function (item) {return item.name === string;});
+}
